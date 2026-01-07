8c67551030f2a6425955231db8bb5e6c22108bef
jennie213 <151994271+jennie213@users.noreply.github.com>
2026-01-07 19:39:26 +0530

    Delete InventoryProject/githubDemo directory

--- InventoryProject/githubDemo/src/main/java/edu/demo/githubDemo/App.java
+++
@@ -1,8 +0,0 @@
-package edu.demo.githubDemo;
-
-public class App {
-   public static void main(String[] args) {
-	  System.out.println("Hello World");
-	  System.out.println("Welcome to Github");
-   }
-}
